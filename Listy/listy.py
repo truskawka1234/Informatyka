c49366cf719f6e912c04793a7f313f3b48b31747
truskawka1234 <mikolajarz@gmail.com>
2023-09-27 18:54:06 +0200

    Dodałem plik listy.py

---
+++ Listy/listy.py
@@ -0,0 +1,102 @@
+        # początek
+# lista = ["A","B","C",10]
+# print(lista)
+
+# print(lista[0])
+
+# print(lista[3])
+
+# print(len(lista))
+
+
+
+        # stworz liste zawierajaca x liczb wprowadzonych przez użytkownika od (1 do 200)
+
+# lista = []
+# while True:
+#     x = (input("x = "))
+#     if x == "stop":
+#         break
+#     if float(x) >=1 and float(x) <= 200:
+#         lista.append(float(x))
+# print(lista)
+
+
+
+
+
+
+
+        # na podstawie otrzymanej listy storz nowa liste gdzie znajduja sie liczby PARZYSTE: wieksze od 50 ale mniejsze od 70.wypisz je na konsoli z informacja ile zawieraja elementow. 
+
+# lista  = []
+# while True:
+#     x = (input("x = "))
+#     if x == "stop":
+#         break
+#     if float(x) >= 50 and float(x)  <= 70 and float(x) %2 == 0:
+#         lista.append (float(x))
+# print(lista)
+# print(len(lista))
+
+
+
+
+
+
+
+        # napisz program co wprowadzi x ocen (1 do 6) a nastepnie obliczy srednia arytmetyczna.jesli ktos wprowadzi inna liczbe niz 1 i 6 niech program wyswietli ,,podales zla liczbe"
+
+# lista = []
+# while True:
+#     x = (input("x = "))
+#     if x == "stop":
+#         break
+#     elif int(x) >=1 and int(x) <= 6:
+#         lista.append(int(x))
+#     else:
+#         print("podales zla liczbe")
+# print(sum(lista)/len(lista))
+
+
+
+        # napisz program do ktorego mozemy wprowadzic dowolne zdanie. niech nasz program wyswietli liczbe samoglosek.
+# a,ą,e,ę,i,o,u,y
+
+
+# x = input("x = ")
+# lista = list(x)    
+# i = 0
+# a = 0
+# ą = 0
+# e = 0
+# ę = 0
+# o = 0
+# u = 0
+# y = 0
+# while i < len (lista):
+#     if lista[i] == "a":
+#         a += 1
+#     elif lista[i] == "ą":
+#         ą += 1
+#     elif lista[i] == "e":
+#         e += 1
+#     elif lista[i] == "ę":
+#         ę += 1
+#     elif lista[i] == "o":
+#         o += 1
+#     elif lista[i] == "u":
+#         u += 1
+#     elif lista[i] == "y":
+#         y += 1
+
+#     i += 1
+# print(lista)
+# print("a = ", a )
+# print("ą = ", ą )
+# print("e = ", e )
+# print("ę = ", ę )
+# print("o = ", o )
+# print("u = ", u )
+# print("y = ", y ) 
+ 
