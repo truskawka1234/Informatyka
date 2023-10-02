026e64d72ac0475cd5ba8df928def5b2be539821
truskawka1234 <mikolajarz@gmail.com>
2023-10-02 17:21:00 +0200

    dodalem folder For.py

---
+++ for/For.py
@@ -0,0 +1,60 @@
+
+# WYSWIETL LICZBY OD 1 DO 10
+# for x in range (11):
+#     print(x)
+
+
+
+
+# OBLICZ SUME LICZB OD 1 DO 100
+# my_sum = 0
+# for num in range (101):
+#     my_sum += num
+# print (my_sum)~~
+
+
+
+
+# WYSWIETL WSZYSTKIE LICZBY PARZYSTE OD 1 DO 50
+# for x in range (1,51):
+#     if x % 2 == 0:
+#         print (x)
+
+
+
+# OBLICZ ILOCZYN LICZB OD 1 DO 5
+# xl = 1
+# for x in range (1,6):
+#     xl *= x
+# print(xl)
+
+
+# Wyświetl wszystkie litery z podanego słowa
+# x = input()
+# for el in x:
+#     print(el)
+
+# WYSWIETL WSZYSTKIE LICZBY OD 20 DO 30 KTORE SA PODZIELNE PRZEZ 3
+# for x in range(20,31):
+#     if x % 3 == 0:
+#         print(x)
+
+
+# WYSWIETL NAJWIEKSZA LICZBE W LISCIE ***
+# lista = [2,1,23,4,3,4,34,234,3,4,3]
+# my_max = lista[0]
+# for el in lista:
+#     if el > my_max:
+#         my_max = el
+# print(my_max)
+
+
+# OBLICZ ŚREDNIA ARYTMETYCZNA LICZB Z LISTY
+# lista = [1,5,5,1,4,2,8,10]
+# suma = 0
+# for el in lista:
+#     suma += el 
+# print(suma/len(lista))    
+
+   
+
