d4572088120d0a9e4790e8b521d62f53fb70807c
truskawka1234 <mikolajarz@gmail.com>
2024-10-28 19:28:17 +0100

    pracka domowa

---
+++ Strony/prostokaty/json/applicationMenu.js
@@ -0,0 +1,14 @@
+const openMemu = () => {
+  console.log("działa");
+};
+
+memu.addEventListener("click", () => {
+  openMemu();
+});
+const openMemu2 = () => {
+  console.log("działa");
+};
+
+memu2.addEventListener("click", () => {
+  openMemu();
+});
---
+++ Strony/prostokaty/json/elements.js
@@ -0,0 +1,2 @@
+const memu = document.querySelector("#menu");
+const memu2 = document.querySelector("#menu2");
