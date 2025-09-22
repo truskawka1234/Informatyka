f120dceb624c9890beb97413d03d5615280ef211
truskawka1234 <mikolajarz@gmail.com>
2025-09-22 23:22:39 +0200

    zrobilem wakacyjny

--- Api/Projekt06/src/components/Header.jsx
+++ Api/Projekt06/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="header__title"> TheCocktails </h1>
+      <h1 className="header__title"> COCKTAIL </h1>
     </header>
   );
 }
